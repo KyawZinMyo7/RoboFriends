18c33b2e6ee5d4dbf3533a1695fdd2c3923f2d67
myogyiyaya <115334169+myogyiyaya@users.noreply.github.com>
2023-01-07 22:51:40 +0900

    Updates

---
+++ precache-manifest.c3930bc54b80a899848f262fd9b420ca.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "35e96ef552a00255ff8e",
+    "url": "/RoboFriends/static/css/main.965e8ce1.chunk.css"
+  },
+  {
+    "revision": "35e96ef552a00255ff8e",
+    "url": "/RoboFriends/static/js/main.91f99c2e.chunk.js"
+  },
+  {
+    "revision": "d2b228f2a7b08db4868a",
+    "url": "/RoboFriends/static/js/runtime~main.6fea91c0.js"
+  },
+  {
+    "revision": "be32f1e94b609390c537",
+    "url": "/RoboFriends/static/css/2.8904d672.chunk.css"
+  },
+  {
+    "revision": "be32f1e94b609390c537",
+    "url": "/RoboFriends/static/js/2.b19faffd.chunk.js"
+  },
+  {
+    "revision": "089bc876b02613239fa5",
+    "url": "/RoboFriends/static/js/3.b7c4cbb6.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RoboFriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "f6eecee0ce803bf8b4175560092adcb2",
+    "url": "/RoboFriends/index.html"
+  }
+];
